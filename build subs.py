25663222d4331ff2ca88dbc1535acfd3a431d187
Jules Gantenbein <hamsolo4744@gmail.com>
2020-04-07 22:04:15 +0800

    Add files via upload

---
+++ build subs.py
@@ -0,0 +1,186 @@
+import pytesseract
+import datetime
+import os
+import time
+import concurrent.futures
+from pprint import pprint
+
+#example file names
+'0_33_59_880__0_34_00_839_00522.jpeg'
+'0_35_00_040__0_35_00_519_00112.jpeg'
+
+# only read images with this extension.
+ext = '.jpeg'
+
+# path with images to read.
+path = os.getcwd()
+
+# name of the srt file to create
+subname = 'Generated subtitles.srt'
+
+# language of the subtitles
+sublang = 'chi_sim'
+#sublang = 'eng'
+
+# Shows the minimum confidence in the sentence
+showMinimumConfidence = True
+
+#example output showMinimumConfidence False
+"""1
+00:02:04,334 --> 00:02:06,709
+I left the village when I was thirteen."""
+
+#example output showMinimumConfidence True
+"""1
+00:02:04,334 --> 00:02:06,709
+(72%)
+I left the village when I was thirteen."""
+
+# If you think grammatical characters or spaces are missing from your subs
+# check the text_fixer, either modify it to your needs or bypass it.
+
+startTime = 0
+
+def gather_subs_tp(path, showConfidence=True, debug=False):
+    global startTime
+    startTime = int(time.time())
+    op = []
+    count = 0
+    namelist = []         
+    for root, dirs, files in os.walk(path):
+        for name in files:
+            if ext in name:
+                namelist.append(name)
+    with concurrent.futures.ThreadPoolExecutor() as executor:
+        for name, result in zip(namelist, executor.map(vcheck, namelist)):
+            if debug >= 1: print('Time Elapsed:',
+                                  str(int(time.time())-startTime),
+                                  'secs. Image:', count, 'Name:',name,
+                                  'minconf:', result['minconf'],
+                                  'text:', result['text'])
+            try:
+                times, start = fn2time(name, debug)
+                if result['text'] == '': raise ValueError
+                if showConfidence:
+                    text = '({1}%)\n{0}'.format(result['text'],
+                                                result['minconf'])
+                else:
+                    text = result['text']
+                op.append((start, times, text))
+                count+=1
+            except KeyboardInterrupt:
+                raise KeyboardInterrupt
+            except:
+                print('Did not run on: ', i)
+    return op
+
+def gather_subs_pp(path, debug=False):
+    global startTime
+    startTime = int(time.time())
+    op = []
+    count = 0
+    namelist = []         
+    for root, dirs, files in os.walk(path):
+        for name in files:
+            if ext in name:
+                namelist.append(name)
+    with concurrent.futures.ProcessPoolExecutor() as executor:
+        for name, result in zip(namelist, executor.map(vcheck, namelist)):
+            try:
+                if debug >= 1: print('Time Elapsed:',
+                                      str(int(time.time())-startTime),
+                                     'secs. Image:', count, 'Name:',name,
+                                     'minconf:', result['minconf'],
+                                     'text', result['text'])
+                times, start = fn2time(name, debug)
+                text = '({1}%)\n{0}'.format(result['text'], result['minconf'])
+                op.append((start, times, text))
+                count+=1
+            except KeyboardInterrupt:
+                raise KeyboardInterrupt
+            except:
+                print('Did not run on: ', i)
+            
+    return op
+
+def check(name, lang=sublang, debug=False):
+    config = ("-l {} --oem 1 --psm 7".format(lang))
+    text = pytesseract.image_to_string(name, config=config)
+    return text
+
+def check1(name, lang=sublang, debug=False):
+    config = ("-l {} --oem 1 --psm 7".format(lang))
+    text = pytesseract.image_to_data(name, config=config).split('\n')
+    op = []
+    for i in text:
+        sp = i.split('\t')
+        op.append(sp[-2:])
+    return op
+
+def vcheck(name, lang=sublang, debug=False):
+    config  = ("-l {} --oem 1 --psm 7".format(lang))
+    result  =  pytesseract.image_to_data(name, config=config).split('\n')[1:]
+    minconf = 100
+    text    = ''
+    for i in result:
+        sp = i.split('\t')
+        t = sp[-1]
+        t = text_fixer(t)
+        if not t == '':
+            conf = int(sp[-2])
+            if conf < minconf and not conf == -1:
+                minconf = conf
+            text = ''.join([text,t])
+    return {'text': text, 'minconf': minconf}
+
+def text_fixer(text, debug=False):
+    replaceMe = ['\n', '“', '”', '、', '//', ';', '<',
+                 '>', '-', '_', '=', '+', '*', '&', '^',
+                 '%', '#','@', '$', '.', '.', '~', ' ', '\t']
+    stripMe = [',', '，',':']
+    for i in replaceMe:
+        text = text.replace(i,'')
+    for i in stripMe:
+        text = text.strip(i)
+    text = text.strip()
+    return text
+
+def list2str(t):
+    return '{}:{}:{},{}'.format(*t)
+
+def fn2time(istr, debug=False):
+    pieces = istr.split('_')
+    if not len(pieces) == 10:
+        raise ValueError('{} Not a set of times, expected 10 elements, got {}'.format(
+                         istr, len(pieces)))
+    start = list2str(pieces[0:4])
+    end   = list2str(pieces[5:9])
+    return '{} --> {}'.format(start, end), list2time(pieces[0:4], debug)
+
+def list2time(ilist, debug=False):
+    if debug >=2: print(ilist)
+    t = (int(i) for i in ilist)
+    return datetime.time(*t)
+
+def sort_subs(subs):
+    return sorted(subs, key=lambda sub: sub[0])
+
+def build_subs(subs, debug=False):
+    op = ''
+    for index, subgroup in enumerate(subs):
+        if debug: print(index, subgroup, [type(i) for i in subgroup])
+        op = '\n'.join([op, str(index+1), *subgroup[1:], ''])
+    return op
+
+def write_subs(path, subtext, debug=False):
+    with open(path, 'w', encoding='utf-8') as f:
+        f.write(subtext)
+    print('Finished writing: {}'.format(path))
+
+if __name__ == '__main__':
+    #pass
+    write_subs(subname, build_subs(sort_subs(gather_subs_tp(path,
+                                                            showMinimumConfidence,
+                                                             debug=1)),
+                                    debug=False))
+    print(str(int(time.time())-startTime))
---
+++ resize_images.py
@@ -0,0 +1,92 @@
+import cv2 as cv
+import numpy as np
+from PIL import Image
+import os
+import time
+import os
+
+#used for resizing, it will resize the image maintaining aspect ratio
+# to the smallest dimension, my images were 5000 by 1000, so it gets shrunk to
+# 40 px tall and an unknown width.
+size = (1920, 40)
+
+# only run on files with this file extension.
+ext = '.jpeg'
+
+# folder to run on.
+path = os.getcwd()
+
+startTime = 0
+
+def get_boundaries(name, debug=False):
+    # crop out unecessary whitespace
+    src = cv.imread(cv.samples.findFile(name),1)
+    src_gray = cv.blur(src, (3,3))
+    threshold = 100
+    leftmost = src.shape[1]
+    rightmost = 0
+    canny_output = cv.Canny(src_gray, threshold, threshold * 2)
+    contours, _ = cv.findContours(canny_output, cv.RETR_EXTERNAL, cv.CHAIN_APPROX_SIMPLE)
+    offset = int(src.shape[1]*0.03)
+    # Get the moments
+    mu = [None]*len(contours)
+    for i in range(len(contours)):
+        mu[i] = cv.moments(contours[i])
+    # Get the mass centers
+    mc = [None]*len(contours)
+    for i in range(len(contours)):
+        # add 1e-5 to avoid division by zero
+        mc[i] = (mu[i]['m10'] / (mu[i]['m00'] + 1e-5), mu[i]['m01'] / (mu[i]['m00'] + 1e-5))
+    # Draw contours
+    minarea = 1000
+    for i, j in enumerate(contours):
+        val = int(mc[i][0])
+        area = cv.contourArea(contours[i])
+        #val = int(i[0][0][0])
+        if leftmost > val and val > offset and area > minarea:
+           leftmost = val
+        if rightmost < val and val < src.shape[1]-offset and area > minarea:
+            rightmost = val
+        if debug >= 2:
+            print('val: ',end='')
+            print(val, end = '')
+            print(' '.join(['Number', str(i), 'lm', str(leftmost),
+                            'rm', str(rightmost)]))
+    leftmost -= offset
+    rightmost += offset
+    # Calculate the area with the moments 00 and compare with the result of the OpenCV function
+    if debug >= 3:
+        for i in range(len(contours)):
+            print(' * Contour[{0}]. Area: {1}. Length: {2}.'.format(i, cv.contourArea(contours[i]), cv.arcLength(contours[i], True), contours[i]))
+        
+    return leftmost, 0, rightmost, src.shape[0]
+
+def run_on(path, debug=False):
+    global startTime
+    startTime = int(time.time())
+    op = []
+    count = 0
+    for root, dirs, files in os.walk(path):
+        for name in files:
+            if ext in name:
+                count+=1
+                if debug >= 1: print('Time Elapsed:',
+                                     str(int(time.time())-startTime),
+                                     'secs. Image:', count, 'Name:', name)
+                shrink_and_crop(name, get_boundaries(name), size, debug)
+    print('Done in {} seconds!'.format(str(int(time.time())-startTime))
+
+def shrink_and_crop(name, cropbox, maxsize, debug=False):
+    im = Image.open(name)
+    if debug >= 2: print(im, cropbox)
+    try:
+        im = im.crop(cropbox)
+        if im.size[1] > maxsize[1]:
+            im.thumbnail(maxsize)
+        if debug >= 2: print(im)
+        im.save(name)
+        #im.save('did it work1.jpg')
+    except ValueError:
+        print('Did not run on:',name)
+
+run_on(path, debug=1)
